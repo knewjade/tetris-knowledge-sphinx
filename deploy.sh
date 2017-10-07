be1df16d907dfcb70ba43a4ce2d2fea1b763d04d
newjade <knewjade@gmail.com>
2017-10-07 19:12:36 +0900

    Improve deploy shell

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
+set -eu
+
 # tetris-knowledgeリポジトリと
 # tetris-knowledge-sphinxリポジトリが
 # 同じ階層に配置されていること
 
 # tetris-knowledge-sphinx直下で実行する
 
+make html
 rm -rf ../tetris-knowledge/*
 cp -r ./build/html/* ../tetris-knowledge/
