80cc109b31254a4eb19b61697a05182ec6c38d1a
newjade <knewjade@gmail.com>
2018-06-22 23:13:53 +0900

    Fix reSt warning / WARNING: Duplicate explicit target name

--- source/conf.py
+++ source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'newjade-tetris-knowledge'
-copyright = '2017, newjade'
+copyright = '2017-2018, newjade'
 author = 'newjade'
 
 # The version info for the project you're documenting, acts as replacement for
